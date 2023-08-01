dae74e335bed93f26ce4f3bf72ed14ad352cf889
Sanghak Oeng <ingsanghak@gmail.com>
2023-08-01 09:29:08 -0400

    first commit

--- client/src/app/components/products-list/products-list.component.ts
+++ client/src/app/components/products-list/products-list.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { StorageService } from 'src/app/_services/storage.service';
+import { ProductService } from 'src/app/services/product.service';
+
+import { map } from 'rxjs/operators';
 
 import { Product } from 'src/app/models/product.model';
 
-import { ProductService } from 'src/app/services/product.service';
-
-
-
-
-
-
+ 
 
 @Component({
 
@@ -24,157 +20,69 @@ import { ProductService } from 'src/app/services/product.service';
 
 export class ProductsListComponent implements OnInit {
 
-
-
   products?: Product[];
 
-  currentProduct: Product = {};
+  currentProduct?: Product;
 
   currentIndex = -1;
 
   name = '';
 
+ 
 
+  constructor(private productService: ProductService) { }
 
-  private roles: string[] = [];
-
-  isLoggedIn = false;
-
-  showAdminBoard = false;
-
-  showModeratorBoard = false;
-
-  username?: string;
-
-
-
-  constructor(private productService: ProductService, private storageService: StorageService,) { }
-
-
+ 
 
   ngOnInit(): void {
 
     this.retrieveProducts();
 
-    this.isLoggedIn = this.storageService.isLoggedIn();
-
-
-
-    if (this.isLoggedIn) {
-
-      const user = this.storageService.getUser();
-
-      this.roles = user.roles;
-
-
-
-      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
-
-      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
-
-
-
-      //this.username = user.username;
-
-    }
-
   }
 
-
-
-
-
-
-
-  retrieveProducts(): void {
-
-    this.productService.getAll()
-
-      .subscribe({
-
-        next: (data) => {
-
-          this.products = data;
-
-          console.log(data);
-
-        },
-
-        error: (e) => console.error(e)
-
-      });
-
-  }
-
-
+ 
 
   refreshList(): void {
 
-    this.retrieveProducts();
-
-    this.currentProduct = {};
+    this.currentProduct = undefined;
 
     this.currentIndex = -1;
 
+    this.retrieveProducts();
+
   }
 
+ 
 
+  retrieveProducts(): void {
+
+    this.productService.getAll().snapshotChanges().pipe(
+
+      map(changes =>
+
+        changes.map(c =>
+
+          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+
+        )
+
+      )
+
+    ).subscribe(data => {
+
+      this.products = data;
+
+    });
+
+  }
+
+ 
 
   setActiveProduct(product: Product, index: number): void {
 
     this.currentProduct = product;
 
     this.currentIndex = index;
-
-  }
-
-
-
-  removeAllProducts(): void {
-
-    this.productService.deleteAll()
-
-      .subscribe({
-
-        next: (res) => {
-
-          console.log(res);
-
-          this.refreshList();
-
-        },
-
-        error: (e) => console.error(e)
-
-      });
-
-  }
-
-
-
-  searchName(): void {
-
-    this.currentProduct = {};
-
-    this.currentIndex = -1;
-
-
-
-    this.productService.findByName(this.name)
-
-      .subscribe({
-
-        next: (data: Product[] | undefined) => {
-
-          this.products = data;
-
-          console.log(data);
-
-        },
-
-        error: (e: any) => console.error(e)
-
-      });
 
   }
 
